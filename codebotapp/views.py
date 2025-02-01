d9d3dcbad3f7056e4297495e1c92e9efda8e7127
Aashish Naik <naikaa@gmail.com>
2025-02-01 17:33:44 -0600

    v 1.0

--- codebotapp/views.py
+++ codebotapp/views.py
@@ -12,6 +12,7 @@ def index(request):
     if request.method == 'POST':
         # Get the input string from the form
         ghtkn = os.getenv("GH_API_TOKEN")
+        print(f"File: {ghtkn}")
         gh_token = Github(ghtkn)
         input_string = request.POST.get('input_string') #repo = "microsoft/vscode"
         # Cache the repo name, gh token, llm token
@@ -64,6 +65,8 @@ def view_pull_request(request, pr_number):
 
 def llm_review(request):
 	gmtkn = os.getenv("GEMINI_API_TOKEN")
+	print("******File:******:")
+	print(gmtkn)
 	genai.configure(api_key= gmtkn)
 	# Create the model
 	generation_config = {
@@ -74,44 +77,48 @@ def llm_review(request):
   		"response_mime_type": "text/plain",
 	}
 
-	model = genai.GenerativeModel(
-  		model_name="gemini-2.0-flash-exp",
-  		generation_config=generation_config,
-	)
-
-	chat_session = model.start_chat(
-  		history=[
-  		]
-	)
 	
-	pr_number = request.POST.get('pull_request_id')
-	pr_number = int(pr_number)
-	
-	if not pr_number:
+	try:
+		model = genai.GenerativeModel(
+			model_name="gemini-2.0-flash-exp",
+			generation_config=generation_config,
+			)
+		chat_session = model.start_chat(history=[])
+		print("LLM session started successfully.")
 
-		return render(request, "error_page.html", {"message": "Pull request ID is missing."})
-	
-	code_repo = cache.get('code_repo')
-	g = cache.get('gh_token')
-	repo = g.get_repo(code_repo)
 
-	pull_request = repo.get_pull(pr_number)
-	files_changed = pull_request.get_files()
 
-	context = {}
-
-	for file in files_changed:
-		print(f"File: {file.filename}")
-		prompt_role_tl = "You are a technical lead reviewing code and providing comments.Be to the point in the response. Please review the following Pull Requests and provide your feedback:"
-		prompt = prompt_role_tl + " " + file.patch
-		#print(prompt)
-		response = chat_session.send_message(prompt)
-		text_response = response.text
-		context[file.filename] = text_response
+		pr_number = request.POST.get('pull_request_id')
+		pr_number = int(pr_number)
 		
-		#print(f"File: {response}")
-	 	
-	print(f"All file items: {context}")
+		if not pr_number:
+
+			return render(request, "error_page.html", {"message": "Pull request ID is missing."})
+		
+		code_repo = cache.get('code_repo')
+		g = cache.get('gh_token')
+		repo = g.get_repo(code_repo)
+
+		pull_request = repo.get_pull(pr_number)
+		files_changed = pull_request.get_files()
+
+		context = {}
+
+		for file in files_changed:
+			print(f"File: {file.filename}")
+			prompt_role_tl = "You are a technical lead reviewing code and providing comments.Be to the point in the response. Please review the following Pull Requests and provide your feedback:"
+			prompt = prompt_role_tl + " " + file.patch
+			#print(prompt)
+			response = chat_session.send_message(prompt)
+			text_response = response.text
+			context[file.filename] = text_response
+			
+			#print(f"File: {response}")
+		 	
+		print(f"All file items: {context}")
+
+	except Exception as e:
+		print(f"Error initializing chat: {e}")
 
 
 
